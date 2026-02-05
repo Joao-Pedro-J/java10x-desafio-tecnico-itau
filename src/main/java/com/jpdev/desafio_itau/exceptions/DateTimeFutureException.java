ebdb1b20a4a52ee72802993cf5357a3ceb80c786
Joao-Pedro-J <matheusitalo125@gmail.com>
2026-02-05 20:05:44 -0300

    feat: Implements exceptions

---
+++ src/main/java/com/jpdev/desafio_itau/exceptions/DateTimeFutureException.java
@@ -0,0 +1,7 @@
+package com.jpdev.desafio_itau.exceptions;
+
+public class DateTimeFutureException extends RuntimeException {
+    public DateTimeFutureException(String message) {
+        super(message);
+    }
+}
